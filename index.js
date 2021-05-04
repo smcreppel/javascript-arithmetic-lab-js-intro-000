498f82f9090a6cd7af47d26695169ee0673bf467
SARA Creppel <saramichellecreppel@gmail.com>
2021-05-04 14:07:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var a, b; 
+
+(function () {
+  a = Math.floor(Math.random() * 1000);
+  b = Math.floor(Math.random() * 1000);
+});
+describe('basic math functions', function () {
+  it ("'add()' is a valid function", function () {
+    expect(add).toExist;
+});
